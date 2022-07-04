6719d5c3e85be82508e757fc8ffaa6ebb07e2c2a
hsb312 <hsb973@naver.com>
2022-07-05 00:17:18 +0900

    #17

---
+++ MSA_UserService/src/test/java/com/posworld/userService/MSA_UserService/MsaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posworld.userService.MSA_UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
