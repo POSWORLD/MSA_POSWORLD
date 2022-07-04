8a75188832d1a752b706cf4527ff41d742345dde
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-07-04 17:47:42 +0900

    first init

---
+++ MSA_Config/msa_config/src/main/java/com/posco/posworld/msa_config/MsaConfigApplication.java
@@ -0,0 +1,13 @@
+package com.posco.posworld.msa_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaConfigApplication.class, args);
+	}
+
+}
