d52f7a039de8e5cc0da896da6299ea0d2d256fa5
wjddls1542 <wjddls1542@scnu.ac.kr>
2022-07-05 15:50:15 +0900

    #24 [backend] insert, delete, update test success

---
+++ MSA_BoardService/boardService/boardService/src/main/java/com/posworld/boardService/board/model/SelectJoinDto.java
@@ -0,0 +1,25 @@
+package com.posworld.boardService.board.model;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Component
+@Entity
+@Table(name="boardtbl")
+@DynamicInsert
+public class SelectJoinDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer num;
+    private Integer homeid;
+    private String content;
+    private Timestamp wdate;
+    private Integer friendid;
+    private String friendimg;
+    private String friendname;
+}
