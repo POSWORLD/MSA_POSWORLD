9ac392e4429b32bfa649e2fb77b8743470872e17
wjddls1542 <wjddls1542@scnu.ac.kr>
2022-07-07 10:16:30 +0900

    gateway update

--- MSA_BoardService/boardService/boardService/src/main/java/com/posworld/boardService/board/repository/BoardJoinRepository.java
+++ MSA_BoardService/boardService/boardService/src/main/java/com/posworld/boardService/board/repository/BoardJoinRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface BoardJoinRepository extends JpaRepository<SelectJoinDto, Integer> {
-    @Query(value = " SELECT b.num, b.homeid, b.content, b.wdate, b.friendid, u.name As friendname, u.prophoto As friendimg FROM boardtbl " +
+    /*@Query(value = " SELECT b.num, b.homeid, b.content, b.wdate, b.friendid, u.name As friendname, u.prophoto As friendimg FROM boardtbl " +
             "as b INNER JOIN usertbl as u ON b.friendid = u.id WHERE homeid= ?", nativeQuery = true)
-    List<SelectJoinDto> getBoards(Integer homeid);
+    List<SelectJoinDto> getBoards(Integer homeid);*/
 
     List<SelectJoinDto> getByHomeid(Integer homeid);
 }
