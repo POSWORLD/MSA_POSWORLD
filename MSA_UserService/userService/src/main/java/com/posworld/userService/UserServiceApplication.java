526eaffd75baf8522e17cf39f2e658d7f72bf63d
hsb312 <hsb973@naver.com>
2022-07-07 14:40:20 +0900

    0607

--- MSA_UserService/userService/src/main/java/com/posworld/userService/UserServiceApplication.java
+++ MSA_UserService/userService/src/main/java/com/posworld/userService/UserServiceApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
